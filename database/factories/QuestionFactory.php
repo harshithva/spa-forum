3c6d51217e247a106267d47dab542af99ac1cadd
harshithva <harshith11032001@gmail.com>
2020-03-26 14:33:03 +0530

    question factory created

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -3,10 +3,20 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model\Question;
+use App\Model\Category;
+use App\Model\User;
 use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title' => $title,
+        'slug' => str_slug($title),
+        'category_id' => function() {
+            return Category::all()->random();
+        },
+        'user_id' => function() {
+            return User::all()->random();
+        }
     ];
 });
