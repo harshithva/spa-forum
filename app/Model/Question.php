fdcfbf67ee828b1b5ce59b20975f736844957243
harshithva <harshith11032001@gmail.com>
2020-03-26 14:24:28 +0530

    question relationship done

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
